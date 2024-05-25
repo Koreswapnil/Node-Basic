39fa1c628c33c9e8ca654cb46edea40f5c90aecd
Swapnil Kore <koreswapnil72@gmail.com>
2024-05-25 10:10:50 +0530

    Node Basic

---
+++ index.js
@@ -0,0 +1,5 @@
+import {generateRandomNumber, celciusToFahrenheit} from './util.js';
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Celcius To Fahrenheit: ${celciusToFahrenheit(-28)}`)
---
+++ util.js
@@ -0,0 +1,7 @@
+export function generateRandomNumber() {
+      return Math.floor(Math.random() * 100) + 1;
+}
+
+export function celciusToFahrenheit(celcius) {
+      return(celcius * 9) / 5 +32
+}
